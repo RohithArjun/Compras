a0b42b044fa48b5909fdae8a79b31d49c9f53ca2
RohithArjun <botturohith605@gmail.com>
2026-07-15 20:40:56 +0530

    CORS error resolved

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import JSEncrypt from "jsencrypt";
-import axios from "./axios";
+import axios from "../axios";
 
 const Login = ({ setCurrentUser }) => {
   const navigate = useNavigate();
